995a52f150aef4c5dc686c44d5b57ed7b7b5f9de
Jason Vance <jbvance@gmail.com>
2016-12-20 17:10:14 -0600

    Checkpoint 10 directives 2: completed

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -14,12 +14,22 @@
             templateUrl: '/templates/directives/seek_bar.html',
             replace: true,
             restrict: 'E',
-            scope: { },
+            scope: { 
+                onChange: '&'
+            },
             link: function(scope, element, attributes) {
                 scope.value = 0;
                 scope.max = 100;
                 
                 var seekBar = $(element);
+                
+                 attributes.$observe('value', function(newValue) {
+                     scope.value = newValue;
+                 });
+
+                 attributes.$observe('max', function(newValue) {
+                     scope.max = newValue;
+                 });                                  
                 
                 var percentString = function(){
                     var value = scope.value;
@@ -34,12 +44,13 @@
                 
                 scope.thumbStyle = function(){
                     return {left: percentString()};
-                }
+                };
                 
                 scope.onClickSeekBar = function(event) {
                     console.log("Calling onClickSeekBar");
                     var percent = calculatePercent(seekBar, event);
                     scope.value = percent * scope.max;
+                    notifyOnChange(scope.value);
                 };
                 
                 scope.trackThumb = function() {                    
@@ -49,6 +60,7 @@
                         console.log("percent = " + percent);
                        scope.$apply(function() {
                             scope.value = percent * scope.max;
+                            notifyOnChange(scope.value);
                         });
                     });
  
@@ -57,7 +69,13 @@
                          $document.unbind('mouseup.thumb');
                     });
                 };
-            }
+                
+                var notifyOnChange = function(newValue) {
+                    if (typeof scope.onChange === 'function') {
+                        scope.onChange({value: newValue});
+                    }
+                };
+            } //end of link function
         };
     }
     
--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function () {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
         var SongPlayer = {};    
                 
         /**
@@ -37,6 +37,12 @@
                 formats: ['mp3'],
                 preload: true
             });
+            
+            currentBuzzObject.bind('timeupdate', function(){
+                $rootScope.$apply(function() {
+                   SongPlayer.currentTime = currentBuzzObject.getTime(); 
+                });
+            });
 
             SongPlayer.currentSong = song;
         };
@@ -56,8 +62,7 @@
         */
         var stopSong = function(){
             currentBuzzObject.stop();
-            SongPlayer.currentSong.playing = null;
-            
+            SongPlayer.currentSong.playing = null;            
         };
         
         /**
@@ -65,6 +70,12 @@
         * @type {Object}
         */
         SongPlayer.currentSong = null;
+        
+         /**
+         * @desc Current playback time (in seconds) of currently playing song
+         * @type {Number}
+         */
+         SongPlayer.currentTime = null;
         
         /**
          * @function play
@@ -130,11 +141,22 @@
                  playSong(song);
            }
       };
+        
+      /**
+      * @function setCurrentTime
+      * @desc Set current time (in seconds) of currently playing song
+      * @param {Number} time
+      */
+       SongPlayer.setCurrentTime = function(time) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setTime(time);
+            }
+      };
          
         return SongPlayer;
     }
  
     angular
          .module('blocJams')
-         .factory('SongPlayer', ['Fixtures', SongPlayer]);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
