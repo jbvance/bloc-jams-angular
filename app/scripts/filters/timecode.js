0e85442506a60709088c51dbca5e46e0ec22891f
Jason Vance <jbvance@gmail.com>
2016-12-20 13:09:18 -0600

    Assignment 11 completed

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -7,19 +7,7 @@
                  return '-:--';
              }
              
-             var wholeSeconds = Math.floor(seconds);
-             var minutes = Math.floor(wholeSeconds / 60);
-             var remainingSeconds = wholeSeconds % 60;
- 
-             var output = minutes + ':';
- 
-             if (remainingSeconds < 10) {
-                 output += '0';   
-             }
- 
-             output += remainingSeconds;
- 
-             return output;
+             return buzz.toTimer(seconds);
          };
      }
 
